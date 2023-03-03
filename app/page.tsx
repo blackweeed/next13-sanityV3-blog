a48bdd27f7e3bffcf182f90cd980c721408768ea
blackweeed <blackweeeed@gmail.com>
2023-03-03 18:31:11 +0100

    implementing sanity backend

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div>
+      <h1 className="text-4xl">Welcome to the Blog</h1>
+    </div>
+  );
+}
---
+++ sanity.cli.ts
@@ -0,0 +1,14 @@
+import {defineCliConfig} from 'sanity/cli'
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET;
+
+export default defineCliConfig({
+  api: {
+    projectId:,
+    dataset:,
+  }
+})
+
+"min 29"
+'https://www.youtube.com/watch?v=x3fCEPFgUSM&t=430s' 
