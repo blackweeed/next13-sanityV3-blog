8a4d59e6877daaae88960684f870e3473ea98d81
blackweeed <blackweeeed@gmail.com>
2023-03-05 19:21:21 +0100

    types

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,41 @@
 import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import PreviewSuspense from "../../components/PreviewSuspense";
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
 
-export default function () {
+const query = groq`
+  *[_type=='post']{
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
+
+export default async function () {
   if (previewData()) {
-    return <div>Preview mode</div>;
+    return (
+      <PreviewSuspense
+        fallback={
+          <div role="status">
+            <p className="text-center text-lg animate-pulse text-[#F7AB0A]">
+              Loading Preview Data ....
+            </p>
+          </div>
+        }
+      >
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
   }
+
+  const posts = await client.fetch(query);
+  console.log(posts);
+
   return (
     <div>
-      <h1>Not in Preview mode</h1>
+      <BlogList posts={posts} />
     </div>
   );
 }
